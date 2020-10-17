de307d0cd3428ce904beae368b0f9b7c0c0120ee
Takayuki Nakata <f.seasons017@gmail.com>
2020-10-18 00:20:22 +0900

    basic cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("bookmark")
+        .subcommand(
+            App::new("add")
+                .about("Add bookmark")
+                .arg(Arg::new("input").index(1).required(true)),
+        )
+        .subcommand(App::new("get").about("Get bookmark"))
+        .get_matches();
+
+    if let Some(ref matches) = matches.subcommand_matches("add") {
+        println!("add: {}", matches.value_of("input").unwrap());
+    } else if let Some(_) = matches.subcommand_matches("get") {
+        println!("get");
+    }
 }
