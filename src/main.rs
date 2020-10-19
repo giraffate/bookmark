d8bd93088671845e49b68d0b719ee9bd81cc3133
Takayuki Nakata <f.seasons017@gmail.com>
2020-10-19 11:51:53 +0900

    implement

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use clap::{App, Arg};
+use dirs;
+use toml;
 
-fn main() {
+use std::collections::HashMap;
+use std::fs::{File, OpenOptions};
+use std::io::{Read, Write};
+
+fn main() -> Result<(), std::io::Error> {
     let matches = App::new("bookmark")
         .subcommand(
             App::new("add")
@@ -10,9 +16,34 @@ fn main() {
         .subcommand(App::new("get").about("Get bookmark"))
         .get_matches();
 
+    let mut file = get_bookmark_file();
+    let mut body = String::new();
+    file.read_to_string(&mut body).unwrap();
+    let mut map: HashMap<&str, &str> = toml::from_str(&body).unwrap();
+
+    let current_dir = std::env::current_dir().unwrap();
     if let Some(ref matches) = matches.subcommand_matches("add") {
-        println!("add: {}", matches.value_of("input").unwrap());
+        let input = matches.value_of("input").unwrap();
+        map.insert(current_dir.to_str().unwrap(), input);
+        let toml = toml::to_string(&map).unwrap();
+        write!(file, "{}", toml)?;
+        println!("succeeded");
     } else if let Some(_) = matches.subcommand_matches("get") {
-        println!("get");
+        if let Some(value) = map.get(current_dir.to_str().unwrap()) {
+            println!("{}", value);
+        }
     }
+
+    Ok(())
+}
+
+fn get_bookmark_file() -> File {
+    let home_dir = dirs::home_dir().unwrap();
+    let target_file = home_dir.join(".bookmark.toml");
+    OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(target_file)
+        .unwrap()
 }
