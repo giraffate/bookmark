6b8c417bff4da550caf4eb5140ef137ad7335ad6
Takayuki Nakata <f.seasons017@gmail.com>
2020-10-19 22:47:40 +0900

    Fix bug

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ use dirs;
 use toml;
 
 use std::collections::HashMap;
-use std::fs::{File, OpenOptions};
+use std::fs::OpenOptions;
 use std::io::{Read, Write};
+use std::path::PathBuf;
 
 fn main() -> Result<(), std::io::Error> {
     let matches = App::new("bookmark")
@@ -16,16 +17,21 @@ fn main() -> Result<(), std::io::Error> {
         .subcommand(App::new("get").about("Get bookmark"))
         .get_matches();
 
-    let mut file = get_bookmark_file();
-    let mut body = String::new();
-    file.read_to_string(&mut body).unwrap();
+    let home_dir = dirs::home_dir().unwrap();
+    let path = home_dir.join(".bookmark.toml");
+    let body = get_bookmark(path.clone());
     let mut map: HashMap<&str, &str> = toml::from_str(&body).unwrap();
-
     let current_dir = std::env::current_dir().unwrap();
     if let Some(ref matches) = matches.subcommand_matches("add") {
         let input = matches.value_of("input").unwrap();
         map.insert(current_dir.to_str().unwrap(), input);
         let toml = toml::to_string(&map).unwrap();
+        let mut file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(path)
+            .unwrap();
         write!(file, "{}", toml)?;
         println!("succeeded");
     } else if let Some(_) = matches.subcommand_matches("get") {
@@ -37,13 +43,14 @@ fn main() -> Result<(), std::io::Error> {
     Ok(())
 }
 
-fn get_bookmark_file() -> File {
-    let home_dir = dirs::home_dir().unwrap();
-    let target_file = home_dir.join(".bookmark.toml");
-    OpenOptions::new()
+fn get_bookmark(path: PathBuf) -> String {
+    let mut file = OpenOptions::new()
         .read(true)
         .write(true)
         .create(true)
-        .open(target_file)
-        .unwrap()
+        .open(path)
+        .unwrap();
+    let mut body = String::new();
+    file.read_to_string(&mut body).unwrap();
+    body
 }
